333d30ec1e2ff3c548f0fe668f55aa2878582647
KESHAV <keshavkumar@KESHAVs-MacBook-Air.local>
2023-11-29 13:30:47 +0530

    result text style name corrected

--- lib/constants.dart
+++ lib/constants.dart
@@ -25,7 +25,7 @@ const kTitleTextStyle = TextStyle(
   fontWeight: FontWeight.bold,
 );
 
-const resultTextStyle = TextStyle(
+const kResultTextStyle = TextStyle(
   color: Color(0xFF24D876),
   fontSize: 22.0,
   fontWeight: FontWeight.bold,
