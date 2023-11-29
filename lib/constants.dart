983cbb318d6694f6094c8e932494eed02963d081
KESHAV <keshavkumar@KESHAVs-MacBook-Air.local>
2023-11-29 13:34:21 +0530

    BMI Text Style added

--- lib/constants.dart
+++ lib/constants.dart
@@ -30,3 +30,8 @@ const kResultTextStyle = TextStyle(
   fontSize: 22.0,
   fontWeight: FontWeight.bold,
 );
+
+const kBMITextStyle = TextStyle(
+  fontSize: 100.0,
+  fontWeight: FontWeight.bold,
+);
